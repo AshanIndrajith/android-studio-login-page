e02be6a9be701869387ff09a09ed373805705941
Ashan Indrajith <103479897+AshanIndrajith@users.noreply.github.com>
2023-02-02 23:32:44 +0530

    update

--- app/src/main/java/com/example/myloginapp/MainActivity.java
+++ app/src/main/java/com/example/myloginapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.myloginapp;
+ package com.example.myloginapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
